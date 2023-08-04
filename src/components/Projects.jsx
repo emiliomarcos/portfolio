cda6924879ad66b59ba36dfe055ead8b0383d8a0
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-03 22:12:47 -0600

    add github icons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,6 @@
 import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Projects() {
   const variants = {
@@ -17,19 +19,19 @@ export default function Projects() {
       <h2>Projects</h2>
       <div>
         <a href='https://dreamwake.ai' target='_blank' rel='noreferrer'><h3>DreamWake AI</h3></a>
-        <p>Full Stack MERN app integrating AI with dreams.<a href='https://github.com/emiliomarcos/dreamwake-ai' target='_blank' rel='noreferrer'>   Code</a></p>
+        <p>Full Stack MERN app integrating AI with dreams.<a className="github-icon" href='https://github.com/emiliomarcos/dreamwake-ai' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} size="xl" /></a></p>
       </div>
       <div>
         <a href='https://thebigwave.surf' target='_blank' rel='noreferrer'><h3>The Big Wave</h3></a>
-        <p>Front End React app to explore surfing spots.<a href='https://github.com/emiliomarcos/the-big-wave' target='_blank' rel='noreferrer'>   Code</a></p>
+        <p>Front End React app to explore surfing spots.<a className="github-icon" href='https://github.com/emiliomarcos/the-big-wave' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} size="xl" /></a></p>
       </div>
       <div>
         <a href='http://www.homie.help' target='_blank' rel='noreferrer'><h3>Homie</h3></a>
-        <p>Team Full Stack Ruby on Rails home products helper.<a href='https://github.com/Lou951/HOMIE' target='_blank' rel='noreferrer'>   Code</a></p>
+        <p>Team Full Stack Ruby on Rails home products helper.<a className="github-icon" href='https://github.com/Lou951/HOMIE' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} size="xl" /></a></p>
       </div>
       <div>
         <a href='https://islands-r-us.herokuapp.com/' target='_blank' rel='noreferrer'><h3>Islands R Us</h3></a>
-        <p>Team Full Stack Ruby on Rails airbnb clone.<a href='https://github.com/BirchAD/Islands-R-Us' target='_blank' rel='noreferrer'>   Code</a></p>
+        <p>Team Full Stack Ruby on Rails airbnb clone.<a className="github-icon" href='https://github.com/BirchAD/Islands-R-Us' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} size="xl" /></a></p>
       </div>
     </motion.div>
   );
