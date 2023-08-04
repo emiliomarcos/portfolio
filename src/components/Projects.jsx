bc658087ae480e361ba997fd2529112fc02002a6
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-03 21:50:02 -0600

    fix project links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,19 +16,19 @@ const Projects = () => {
     >
       <h2>Projects</h2>
       <div>
-        <a href='https://dreamwake.ai'><h3>DreamWake AI</h3></a>
+        <a href='https://dreamwake.ai' target='_blank' rel='noreferrer'><h3>DreamWake AI</h3></a>
         <p>Full Stack MERN app integrating AI with dreams.</p>
       </div>
       <div>
-        <a href='https://thebigwave.surf'><h3>The Big Wave</h3></a>
+        <a href='https://thebigwave.surf' target='_blank' rel='noreferrer'><h3>The Big Wave</h3></a>
         <p>Front End React app to explore surfing spots.</p>
       </div>
       <div>
-        <a href='www.homie.help'><h3>Homie</h3></a>
+        <a href='http://www.homie.help' target='_blank' rel='noreferrer'><h3>Homie</h3></a>
         <p>Team Full Stack Ruby on Rails home products helper.</p>
       </div>
       <div>
-        <a href='https://islands-r-us.herokuapp.com/'><h3>Islands R Us</h3></a>
+        <a href='https://islands-r-us.herokuapp.com/' target='_blank' rel='noreferrer'><h3>Islands R Us</h3></a>
         <p>Team Full Stack Ruby on Rails airbnb clone.</p>
       </div>
     </motion.div>
