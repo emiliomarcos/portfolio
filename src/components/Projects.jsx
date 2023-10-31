a0fc2deb7dc1b055108b779ee6e057e134cb572c
Emilio Marcos <emilio7marcos@gmail.com>
2023-10-31 17:49:34 -0600

    add yoving

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,6 +26,10 @@ export default function Projects() {
         <p>Front End React app to explore surfing spots.<a className="github-icon" href='https://github.com/emiliomarcos/the-big-wave' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} size="xl" /></a></p>
       </div>
       <div>
+        <a href='https://yoving.com' target='_blank' rel='noreferrer'><h3>Yoving</h3></a>
+        <p>Work in Progress MERN travel experiences AI assitant.<a className="github-icon" href='https://github.com/emiliomarcos/yoving' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} size="xl" /></a></p>
+      </div>
+      <div>
         <a href='http://www.homie.help' target='_blank' rel='noreferrer'><h3>Homie</h3></a>
         <p>Team Full Stack Ruby on Rails home products helper.<a className="github-icon" href='https://github.com/Lou951/HOMIE' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} size="xl" /></a></p>
       </div>
