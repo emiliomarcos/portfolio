6bdbf1485535d90ab3f1a44e0f17f05ad34b2b58
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-02 13:58:51 -0600

    add projects data file

---
+++ src/data/Projects.js
@@ -0,0 +1,39 @@
+const projects = [
+  {
+    name: 'Project name',
+    description: 'Project description',
+    link: 'Project link',
+    image: 'Project image',
+    technologies: 'Project technologies'
+  },
+  {
+    name: 'Project name',
+    description: 'Project description',
+    link: 'Project link',
+    image: 'Project image',
+    technologies: 'Project technologies'
+  },
+  {
+    name: 'Project name',
+    description: 'Project description',
+    link: 'Project link',
+    image: 'Project image',
+    technologies: 'Project technologies'
+  },
+  {
+    name: 'Project name',
+    description: 'Project description',
+    link: 'Project link',
+    image: 'Project image',
+    technologies: 'Project technologies'
+  },
+  {
+    name: 'Project name',
+    description: 'Project description',
+    link: 'Project link',
+    image: 'Project image',
+    technologies: 'Project technologies'
+  }
+]
+
+export default projects;
