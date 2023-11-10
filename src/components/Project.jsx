753f4eee925844a23998532fdaa640560e56dacf
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-10 15:17:43 -0600

    project container with bottom margin

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,11 +4,11 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 // eslint-disable-next-line react/prop-types
 export default function Project({ name, description, link, code, image, technologies }) {
   return (
-    <>
+    <div className="project">
       <a href={link} target='_blank' rel='noreferrer'><h3>{name}</h3></a>
       <p>{description}<a className="github-icon" href={code} target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} size="xl" /></a></p>
       <img className='project-image' src={image} />
       <p>{technologies}</p>
-    </>
+    </div>
   )
 }
