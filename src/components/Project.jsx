b8ea3ce253ce681a688cefb52ba53fb7523d95de
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-10 15:01:31 -0600

    project component props

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,12 @@
-export default function Project() {
+export default function Project({ name, description, link, code, image, technologies }) {
   return (
     <>
-      <h2>Name</h2>
-      <p>description.......</p>
-      <img />
-      <p>technolgies</p>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <img src={image}/>
+      <p>{technologies}</p>
+      <p>{link}</p>
+      <p>{code}</p>
     </>
   )
 }
