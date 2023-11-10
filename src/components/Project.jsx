ebba0553e79bd771bb665c2417f1fa3290b170c2
Emilio Marcos <emilio7marcos@gmail.com>
2023-11-10 15:11:44 -0600

    link title to projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,10 @@
 export default function Project({ name, description, link, code, image, technologies }) {
   return (
     <>
-      <h2>{name}</h2>
+      <a href={link} target='_blank' rel='noreferrer'><h3>{name}</h3></a>
       <p>{description}</p>
       <img className='project-image' src={image} />
       <p>{technologies}</p>
-      <p>{link}</p>
       <p>{code}</p>
     </>
   )
